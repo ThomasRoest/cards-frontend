7c88307990a6b13d307faf531e593a62164db303
Thomas Roest <ThomasRoest@users.noreply.github.com>
2019-03-08 15:14:53 +0100

    update font-size

--- src/components/app/style.tsx
+++ src/components/app/style.tsx
@@ -4,7 +4,7 @@ export const GlobalStyle = createGlobalStyle`
   html {
     box-sizing: border-box;
     font-family: Arial, Helvetica, sans-serif;
-    font-size: 16px;
+    font-size: 18px;
   }
   *,
   *:before,
